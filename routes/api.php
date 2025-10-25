3ce620e33106fd5439256f7305830d978f12e480
winner00kambale <vainqueurkambale@gmail.com>
2025-10-25 14:37:03 +0200

    repport commit

--- routes/api.php
+++ routes/api.php
@@ -150,7 +150,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::controller(ReportController::class)->group(function () {
-        Route::post('/rapport.borne', 'rapportBorne');
+        Route::get('/rapport.borne', 'rapportBorne');
         Route::get('/rapport.point-eau-abonne', 'rapportPointEauAbonne');
         Route::get('/rapport.facturations', 'rapportFacturations');
     });
