eab2143ae5caa506894619819d173ea346efb536
winner00kambale <vainqueurkambale@gmail.com>
2025-10-31 19:45:43 +0200

    payement maintanance commit

--- app/Http/Controllers/Api/Facturation/FacturationController.php
+++ app/Http/Controllers/Api/Facturation/FacturationController.php
@@ -38,8 +38,8 @@ class FacturationController extends Controller
             CASE 
                 WHEN status = 'impayé'  THEN 1
                 WHEN status = 'acompte' THEN 2
-                WHEN status = 'insoldée' THEN 3
-                WHEN status = 'payé'    THEN 4
+                WHEN status = 'payé' THEN 3
+                WHEN status = 'insoldée'    THEN 4
                 ELSE 5
             END
         ")
