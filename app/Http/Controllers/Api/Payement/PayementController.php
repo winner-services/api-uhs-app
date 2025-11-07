c41600c2f4e5b5c4d141b48b4405506f3eebac97
winner00kambale <vainqueurkambale@gmail.com>
2025-11-07 11:47:21 +0200

    bornier commit

--- app/Http/Controllers/Api/Payement/PayementController.php
+++ app/Http/Controllers/Api/Payement/PayementController.php
@@ -267,12 +267,14 @@ class PayementController extends Controller
                 'facturation_id'   => $request->facture_id
             ]);
 
-            // $client_name = $data = Payement::join('tresoreries', 'payements.account_id', '=', 'tresoreries.id')
-            // ->join('facturations', 'payements.facture_id', '=', 'facturations.id')
-            // ->join('point_eau_abonnes', 'facturations.point_eau_abonnes_id', '=', 'point_eau_abonnes.id')
-            // ->join('abonnes', 'point_eau_abonnes.abonne_id', '=', 'abonnes.id')
-            // ->select('payements.*', 'abonnes.nom as abonne', 'users.name as addedBy', 'tresoreries.designation as tresorerie')
-            // ->latest()
+            $client_name = Payement::join('tresoreries', 'payements.account_id', '=', 'tresoreries.id')
+                ->join('facturations', 'payements.facture_id', '=', 'facturations.id')
+                ->join('point_eau_abonnes', 'facturations.point_eau_abonnes_id', '=', 'point_eau_abonnes.id')
+                ->join('abonnes', 'point_eau_abonnes.abonne_id', '=', 'abonnes.id')
+                ->where('payements.facture_id', $payement->facture_id)
+                ->select('abonnes.nom as abonne')
+                ->value('abonne'); // retourne directement le nom (string)
+
 
             DB::commit();
 
@@ -282,6 +284,7 @@ class PayementController extends Controller
                 'status'  => 201,
                 'data'    => $payement,
                 'agent_name' => $user->name,
+                'client_name' => $client_name,
                 'company_info' => $about
             ], 201);
         } catch (\Exception $e) {
