a25264ccf5592dd0716c324e2148958e8e4fdc66
winner00kambale <vainqueurkambale@gmail.com>
2025-11-13 14:14:16 +0200

    produits commit

---
+++ database/migrations/2025_11_13_100304_create_entrees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entrees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantite');
+            $table->decimal('prix_unit_achat',10,2)->nullable();
+            $table->foreignId('product_id')->nullable()->constrained('produits')->nullOnDelete();
+            $table->foreignId('addedBy')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrees');
+    }
+};
