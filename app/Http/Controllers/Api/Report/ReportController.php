12bc4ea767387f7e4cd0068dc25975e9b6d6aec8
winner00kambale <vainqueurkambale@gmail.com>
2025-11-15 08:32:54 +0200

    transct commit

--- app/Http/Controllers/Api/Report/ReportController.php
+++ app/Http/Controllers/Api/Report/ReportController.php
@@ -462,7 +462,7 @@ class ReportController extends Controller
                 ) AS stock_before_start
             ")
             )
-            ->where('date_transaction', '<', $date_start)
+            ->whereDate('date_transaction', '<', $date_start)
             ->groupBy('product_id');
 
         $lastInit = DB::table('logistiques')
