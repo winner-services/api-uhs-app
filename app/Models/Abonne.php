e9c39aef52dcd2d97bb088015ddadcf3007bf583
winner00kambale <vainqueurkambale@gmail.com>
2025-11-16 12:11:45 +0200

    sortie et entree commit

--- app/Models/Abonne.php
+++ app/Models/Abonne.php
@@ -34,7 +34,8 @@ class Abonne extends Model
         $query->where(function ($query) use ($term) {
             $query->where('abonnes.nom', 'like', $term)
                 ->orWhere('abonnes.adresse', 'like', $term)
-                ->orWhere('abonnes.statut', 'like', $term)
+                ->orWhere('abonnes.status', 'like', $term)
+                ->orWhere('abonnes.num_piece_identite', 'like', $term)
                 ->orWhere('abonnes.genre', 'like', $term)
                 ->orWhereHas('categorie', function ($q2) use ($term) {
                     $q2->where('abonnement_categories.designation', 'like', $term);
