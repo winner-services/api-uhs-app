979feea6a3589dcfd0471ea92549a300755f6e7a
winner00kambale <vainqueurkambale@gmail.com>
2025-11-23 07:01:37 +0200

    facturation commit

--- app/Models/PointEauAbonne.php
+++ app/Models/PointEauAbonne.php
@@ -30,10 +30,10 @@ class PointEauAbonne extends Model
         $term = "%$term%";
         $query->where(function ($query) use ($term) {
             $query->where('point_eau_abonnes.date_operation', 'like', $term)
-                ->orWhereHas('pointEauAbonne.pointEau', function ($q1) use ($term) {
+                ->orWhereHas('pointEau', function ($q1) use ($term) {
                     $q1->where('point_eaus.matricule', 'like', $term);
                 })
-                ->orWhereHas('pointEauAbonne.abonne', function ($q2) use ($term) {
+                ->orWhereHas('abonne', function ($q2) use ($term) {
                     $q2->where('abonnes.nom', 'like', $term);
                 });
         });
